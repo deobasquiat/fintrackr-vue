6ebef2699b7cae408a89056314e513e1a7f4db7b
Tiago Calado <1tiagocalado8@gmail.com>
2023-06-19 17:08:43 +0100

    [Routes] Update routes file

--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,54 @@ Route::middleware([
     'verified',
 ])->group(function () {
 
+    /**
+     * Dashboard
+     */
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
+    /**
+     * Banking Accounts
+     *
+     *  */
+    // Index
     Route::get('/accounts', [AccountsController::class, 'index'])->name('accounts.index');
 
+    // Create
+    Route::post('/accounts/create', [AccountsController::class, 'create'])->name('accounts.create');
+
+    // Show
+    Route::get('/accounts/{id}', [AccountsController::class, 'show'])->name('accounts.show');
+
+    // Edit
+    Route::get('/accounts/{id}/edit', [AccountsController::class, 'edit'])->name('accounts.edit');
+    // Update
+    Route::put('/accounts/{id}', [AccountsController::class, 'update'])->name('accounts.update');
+
+    // Delete
+    Route::delete('/accounts/{account}', [AccountsController::class, 'destroy'])->name('accounts.destroy');
+
+    /**
+     * Transactions
+     *
+     */
+
+    // Index
+    Route::get('/accounts/{id}/transactions', [TransactionsController::class, 'index'])->name('transactions.index');
+
+    // Create
+    Route::post('/accounts/{id}/transactions/create', [TransactionsController::class, 'create'])->name('transactions.create');
+
+    /**
+     * Category
+     *
+     */
+
+    // Index
+    Route::get('/category', [CategoriesController::class, 'index'])->name('categories.index');
+
+    // Create
+    Route::post('/category/create', [CategoriesController::class, 'create'])->name('categories.create');
+
+    // Delete
+    Route::delete('/category/{category}', [CategoriesController::class, 'destroy'])->name('categories.destroy');
 });
